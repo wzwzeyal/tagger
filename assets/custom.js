22680c8bb9fc8c86b989b3910163d8ad22dee387
Ub_eyal <wzwzeyal@gmail.com>
2022-05-17 23:36:09 +0300

    fix annotate status

--- assets/custom.js
+++ assets/custom.js
@@ -1,15 +1,15 @@
 document.addEventListener('keyup', e => {
 
-    console.log("addEventListener - keyup - start")
+    // console.log("addEventListener - keyup - start")
 
 
     let pid = e.target.id;
 
-    console.log(pid)
+    // console.log(pid)
 
     if (pid === "search") {
 
-        console.log("search start");
+        // console.log("search start");
 
         var input, filter, table, tr, td, i, txtValue, col;
         input = document.getElementById("search");
@@ -49,54 +49,11 @@ document.addEventListener('keyup', e => {
             }
         }
     }
-    console.log("addEventListener - keyup - stop")
+    // console.log("addEventListener - keyup - stop")
 
 
 });
 
-$(function() {
-	const rowsPerPage = 13;
-	const rows = $('#dataset-table tbody tr');
-	const rowsCount = rows.length;
-	const pageCount = Math.ceil(rowsCount / rowsPerPage); // avoid decimals
-	const numbers = $('#numbers');
-
-	// Generate the pagination.
-	for (var i = 0; i < pageCount; i++) {
-		numbers.append('<li><a href="#">' + (i+1) + '</a></li>');
-	}
-
-	// Mark the first page link as active.
-	$('#numbers li:first-child a').addClass('active');
-
-	// Display the first set of rows.
-	displayRows(1);
-
-	// On pagination click.
-	$('#numbers li a').click(function(e) {
-		var $this = $(this);
-
-		e.preventDefault();
-
-		// Remove the active class from the links.
-		$('#numbers li a').removeClass('active');
-
-		// Add the active class to the current link.
-		$this.addClass('active');
-
-		// Show the rows corresponding to the clicked page ID.
-		displayRows($this.text());
-	});
-
-	// Function that displays rows for a specific page.
-	function displayRows(index) {
-		var start = (index - 1) * rowsPerPage;
-		var end = start + rowsPerPage;
-
-		// Hide all rows.
-		rows.hide();
-
-		// Show the proper rows for this page.
-		rows.slice(start, end).show();
-	}
+document.addEventListener('console.log("addEventListener - keyup - stop")', e => {
+    console.log("***input***")
 });

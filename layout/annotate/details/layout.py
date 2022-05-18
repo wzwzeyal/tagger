77cb238dc02e2d04421aecfaa432132d9c166ab7
Your Name <you@example.com>
2022-05-18 15:32:05 +0300

    add buttons

--- layout/annotate/details/layout.py
+++ layout/annotate/details/layout.py
@@ -4,31 +4,33 @@ import dash_bootstrap_components as dbc
 details_layout = html.Div(
     [
         dbc.Textarea(id="left-textarea-example", placeholder="1", readonly=True,
-                     style={'height': '40%', 'direction': 'rtl', }
+                     style={'height': '45%', 'direction': 'rtl', }
                      ),
         html.Div(style={'height': '1rem'}),
         dbc.Textarea(id="right-textarea-example", placeholder="1", readonly=True,
-                     style={'height': '40%', 'direction': 'rtl'}
+                     style={'height': '45%', 'direction': 'rtl'}
                      ),
         html.Div(style={'height': '1rem'}),
         html.Div(
             [
-                html.Span(
+                html.Div(
                     [
-                        "Text1",
+                        html.Span(
+                            "Text1",
+                            id="Text1",
+                        ),
                         dcc.Clipboard(
                             target_id="Text1",
                             style={
                                 "position": "absolute",
                                 "top": 2,
-                                "right": 2,
+                                "right": 4,
                                 "fontSize": 20,
                             },
                         ),
                     ],
-                    id="Text1",
                     className="flex-1-border",
-                    style={"position": "relative", }),
+                    style={"position": "relative"}),
                 html.Span("Text2", id="Text2", className="flex-1-border", ),
                 html.Span("Text3", id="Text3", className="flex-1-border", ),
 
@@ -41,7 +43,6 @@ details_layout = html.Div(
                 'align-content': 'start',
             }
         ),
-
 
     ],
     id="details_layout",
